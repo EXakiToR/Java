af445911dbaacecf0893fa789438f2744ea50ae8
EXakiToR <83112883+EXakiToR@users.noreply.github.com>
2023-05-21 14:27:11 +0300

    Optimized

---
+++ CarParking/CarParking/Main.java
@@ -0,0 +1,18 @@
+package CarParking;
+
+public class Main {
+    public static void main(String[] args) {
+        Parking p1 = new Parking((byte) 6);
+        Car c1 = new Car("BMW", "ABC-456");
+        Car c2 = new Car("Fiat", "TAR-963");
+        Car c3 = new Car("Mercedes", "UYI-128");
+        p1.showFreePlaces();
+        p1.parkCar(c1, "1A");
+        p1.parkCar(c2, "2C");
+        p1.parkCar(c2, "2C");
+        p1.parkCar(c1, "2F");
+        p1.parkCar(c3, "2B");
+        System.out.println(p1);
+        p1.showFreePlaces();
+    }
+}
