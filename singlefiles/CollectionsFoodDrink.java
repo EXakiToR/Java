4b6cb30d457dc2208e0d29334421347c80529cae
EXakiToR <83112883+EXakiToR@users.noreply.github.com>
2023-07-02 20:43:33 +0300

    Add files via upload

---
+++ singlefiles/CollectionsFoodDrink.java
@@ -0,0 +1,64 @@
+package singlefiles;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionsFoodDrink {
+    public static void main(String[] args) {
+        List<Product> products = new LinkedList<>();
+        products.add(new Food("Fille Minion", 15, 300));
+        products.add(new AlcDrink("Aperitif", 5, 100));
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println(products.get(i));
+        }
+    }
+}
+
+class Product {
+    private String name;
+    private Integer price;
+
+    public Product() {
+
+    }
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + "]";
+    }
+
+}
+
+class Food extends Product {
+    private Integer weight;
+
+    public Food(String name, Integer price, Integer weight) {
+        super(name, price);
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", weight=" + weight + "]";
+    }
+
+}
+
+class AlcDrink extends Product {
+    private Integer volume;
+
+    public AlcDrink(String name, Integer price, Integer volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", weight=" + volume + "]";
+    }
+}
