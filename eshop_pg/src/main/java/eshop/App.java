a251c1769e9e9d5370c40370c67fc76dcb2c0826
EXakiToR <83112883+EXakiToR@users.noreply.github.com>
2023-09-18 01:10:10 +0300

    Update App.java

--- eshop_pg/src/main/java/eshop/App.java
+++ eshop_pg/src/main/java/eshop/App.java
@@ -11,5 +11,8 @@ public class App {
         DummyEntity de = new DummyEntity("Testing dummy repository with dummy entity.");
         dr.create(de);
         System.out.println(dr.read(de));
+        de.setName("Testing dummy repository with UPDATED dummy entity.");
+        dr.update(de);
+        // dr.delete(de);
     }
 }
